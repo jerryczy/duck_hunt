e012feaeeb9c47579303b3e185d52fa33aa48390
Shukuxe <possumofawesum@hotmail.com>
2017-03-18 13:34:08 -0400

    Update duck_hunt.v
    Basic code for shifter; load_en and shift_en will come from FSM control. The loadnumber will come from a RNG.

--- duck_hunt.v
+++ duck_hunt.v
@@ -1,3 +1,21 @@
 module duck_hunt();
 
 endmodule;
+
+module bird_shifter(clock, load_en, loadnumber, shift_en, s);
+  input clock, load_en;
+  input [19:0] loadnumber;
+  output s;
+  
+  assign s = shifter;
+  
+  reg [19:0] shifter; //Figure out length later.
+  always @(posedge clock) begin
+    if (load)
+      shifter <= loadnumber;
+    else if (shift_en)
+      shifter <= shifter >> 1;
+  end
+   
+    
+endmodule
