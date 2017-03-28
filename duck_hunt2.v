1a5652039be8621531891c57536f734db5063c08
jerryczy <czydu@hotmail.com>
2017-03-28 17:32:10 -0400

    add control and data path fsm

--- duck_hunt2.v
+++ duck_hunt2.v
@@ -1,3 +1,28 @@
+/* FSM template
+
+	always @(*)
+	begin
+		case (current_state)
+			ERASE_BIRDS_1: ;
+			DRAW_BIRDS_1: ;
+			ERASE_BIRDS_2: ;
+			DRAW_BIRDS_2: ;
+			ERASE_BIRDS_3: ;
+			DRAW_BIRDS_3: ;
+			ERASE_BIRDS_4: ;
+			DRAW_BIRDS_4: ;
+			ERASE_BIRDS_5: ;
+			DRAW_BIRDS_5: ;
+			ERASE_BIRDS_6: ;
+			DRAW_BIRDS_6: ;
+			ERASE_HUNTER: ;
+			DRAW_HUNTER: ;
+			DRAW_LASER: ;
+			ERASE_LASER: ;
+		endcase
+	end
+*/
+
 module duck_hunt(CLOCK_50, KEY,
 		VGA_CLK,   						//	VGA Clock
 		VGA_HS,							//	VGA H_SYNC
@@ -22,14 +47,104 @@ module duck_hunt(CLOCK_50, KEY,
 	
 	wire [7:0] x_out;
 	wire [6:0] y_out;
-	wire frame_reached;
+	wire frame_reached, one_frame, reset_f_counter, reset;
+	wire done_draw_1;
 	
 	assign frame_reached = KEY[1];
+	assign reset = KEY[0];
 	
+	reg [4:0] current_state, next_state;
 	
-	bird b1(.cclock(frame_reached), .dclock(CLOCK_50), .reset_counter(1'b0), .x_out(x_out), .y_out(y_out));
+	localparam 	HOLD = 5'd0;
+			ERASE_BIRD_1 = 5'd1;
+			DRAW_BIRD_1 = 5'd2;
+	/*
+	Control
+	*/
+	always @(posedge CLOCK_50)
+	begin
+		case(current_state)
+			HOLD: 		next_state = one_frame ? ERASE_BIRD_1 : HOLD;
+			ERASE_BIRD_1: 	next_state = done_draw_1 ? DRAW_BIRD_1: ERASE_BIRD_1;
+			DRAW_BIRD_1:	next_state = done_draw_1 ? HOLD /*ERASE_BIRDS_2*/ : DRAW_BIRDS_1;
+// 			ERASE_BIRDS_2: next_state = done_draw_2 ? DRAW_BIRDS_2 : ERASE_BIRDS_2;
+// 			DRAW_BIRDS_2: next_state = done_draw_2 ? ERASE_BIRDS_3 : DRAW_BIRDS_2;
+// 			ERASE_BIRDS_3: next_state = done_draw_3 ? DRAW_BIRDS_3 : ERASE_BIRDS_3;
+// 			DRAW_BIRDS_3: next_state = done_draw_3 ? ERASE_BIRDS_4 : DRAW_BIRDS_3;
+// 			ERASE_BIRDS_4: next_state = done_draw_4 ? DRAW_BIRDS_4 : ERASE_BIRDS_4;
+// 			DRAW_BIRDS_4: next_state = done_draw_4 ? ERASE_BIRDS_5 : DRAW_BIRDS_4;
+// 			ERASE_BIRDS_5: next_state = done_draw_5 ? DRAW_BIRDS_5 : ERASE_BIRDS_5;
+// 			DRAW_BIRDS_5: next_state = done_draw_5 ? ERASE_BIRDS_6 : DRAW_BIRDS_5;
+// 			ERASE_BIRDS_6: next_state = done_draw_6 ? DRAW_BIRDS_6 : ERASE_BIRDS_6;
+// 			DRAW_BIRDS_6: next_state = done_draw_6 ? ERASE_HUNTER : DRAW_BIRDS_6;
+// 			ERASE_HUNTER: next_state = done_draw_h ? DRAW_HUNTER : ERASE_HUNTER;
+// 			DRAW_HUNTER: next_state = done_draw_h ? (shoot ? DRAW_LASER : HOLD) : DRAW_HUNTER;
+// 			DRAW_LASER: next_state = done_draw_l ? ERASE_LASER : DRAW_LASER;
+// 			ERASE_LASER: next_state = done_draw_l ? HOLD : ERASE_LASER;
+			default: next_state = HOLD;
+		endcase
+	end
+	
+	always @(posedge CLOCK_50 || posedge reset)
+	begin
+// 		if (posedge reset)
+// 			current_state = HOLD;
+// 		else
+			current_state = next_state;
+	end
+	
+	/*
+	Datapath
+	*/
+	
+	always @(*)
+	begin
+		case (current_state)
+			ERASE_BIRDS_1: colour = 3'b000;
+			DRAW_BIRDS_1: colour = 3'b111;
+// 			ERASE_BIRDS_2: colour = 3'b000;
+// 			DRAW_BIRDS_2: colour = 3'b111;
+// 			ERASE_BIRDS_3: colour = 3'b000;
+// 			DRAW_BIRDS_3: colour = 3'b111;
+// 			ERASE_BIRDS_4: colour = 3'b000;
+// 			DRAW_BIRDS_4: colour = 3'b111;
+// 			ERASE_BIRDS_5: colour = 3'b000;
+// 			DRAW_BIRDS_5: colour = 3'b111;
+// 			ERASE_BIRDS_6: colour = 3'b000;
+// 			DRAW_BIRDS_6: colour = 3'b111;
+// 			ERASE_HUNTER: colour = 3'b000;
+// 			DRAW_HUNTER: colour = 3'b001;
+// 			DRAW_LASER: colour = 3'b010;
+// 			ERASE_LASER: colour = 3'b000;
+		endcase
+	end
+	
+	always @(*)
+	begin
+		case (current_state)
+			ERASE_BIRDS_1: reset_f_counter = 1'b0;
+			DRAW_BIRDS_1: reset_f_counter = 1'b1;
+// 			ERASE_BIRDS_2: ;
+// 			DRAW_BIRDS_2: ;
+// 			ERASE_BIRDS_3: ;
+// 			DRAW_BIRDS_3: ;
+// 			ERASE_BIRDS_4: ;
+// 			DRAW_BIRDS_4: ;
+// 			ERASE_BIRDS_5: ;
+// 			DRAW_BIRDS_5: ;
+// 			ERASE_BIRDS_6: ;
+// 			DRAW_BIRDS_6: ;
+// 			ERASE_HUNTER: ;
+// 			DRAW_HUNTER: ;
+// 			DRAW_LASER: ;
+// 			ERASE_LASER: ;
+		endcase
+	end
+	
+	bird b1(.cclock(frame_reached), .dclock(CLOCK_50), .reset_counter(1'b0), .x_out(x_out), .y_out(y_out), .done(done_draw_1));
 //	
 //	frame_counter f15(.num(6'b001111), .clock(CLOCK_50), .reset(1'b0), .q(frame_reached));
+	frame_counter f1(.num(6'b000001), .clock(CLOCK_50), .reset(reset_f_counter), .q(one_frame));
 
 	vga_adapter VGA(
 		.resetn(KEY[0]),
@@ -54,12 +169,13 @@ module duck_hunt(CLOCK_50, KEY,
 		
 endmodule
 
-module bird(cclock, dclock, reset_counter, x_out, y_out);
+module bird(cclock, dclock, reset_counter, x_out, y_out, done);
 	//cclock = clock for bird_counter
 	//dclock = clock for draw_bird
 	input cclock, dclock, reset_counter;
 	output [7:0] x_out;
 	output [6:0] y_out;
+	output done;
 
 	wire [7:0] x;
 	wire [6:0] y = 7'b0000111;
@@ -81,7 +197,8 @@ module bird(cclock, dclock, reset_counter, x_out, y_out);
 		.x(x),
 		.y(y), 
 		.x_out(x_out), 
-		.y_out(y_out));
+		.y_out(y_out),
+		.done(done));
 		
 endmodule
 
@@ -90,7 +207,8 @@ module draw_bird(
 		input [7:0] x,
 		input	[6:0] y,
 		output reg [7:0] x_out,
-		output reg [6:0] y_out
+		output reg [6:0] y_out,
+		output done
 		);
 		
 	reg [3:0] current_state = BIRD_0;
@@ -177,6 +295,9 @@ module draw_bird(
 //		else
 			current_state <= next_state;
 	end
+	
+	assign done = (current_state == END || ~draw_en) ? 1 : 0;
+	
 endmodule
 
 module bird_counter(clock, reset, enable, new_x);
@@ -232,11 +353,11 @@ module delay_counter(clock, reset, q);
 	input reset;
 	output reg [19:0] q = 0;
 	
-	always @(posedge clock)
+	always @(posedge clock | posedge reset)
 	begin
-		if (reset == 1'b0)
+		if (reset)
 			q <= 0;
-		else if (clock == 1'b1)
+		else
 			begin
 				if (q == 0)
 					q <= 20'b11001011011100110110;//(1/60s)
